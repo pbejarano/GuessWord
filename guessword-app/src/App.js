86dbc49fb4c0236144e548b22531feec44826406
Paul Bejarano <pbejarano88@gmail.com>
2022-03-04 13:11:28 -0700

    Init repo

---
+++ guessword-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Guess Word<br />
+          Adivina la Palabra
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
